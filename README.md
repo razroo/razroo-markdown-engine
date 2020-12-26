82b29425948493c156fcb46f5d74ebf35c0efadb
Charlie Greenman <charlie@razroo.com>
2020-12-26 22:58:20 +0200

    initial commit.

---
+++ README.md
@@ -0,0 +1,50 @@
+<p align="center">
+  <a href="https://www.razroo.com">
+    <img alt="Razroo Logo" src="https://assets.razroo.com/razroo_logo_8f8785be23.svg" width="200" />
+  </a>
+</p>
+
+# Razroo Markdown Engine #
+The Razroo Markdown Engine is Razroo's attempt towards making markdown something that is more automated 
+and has more sophisticated features available by type setting languages such as LaTeX. We wanted it to be written in 
+Markdown, and-also compile to Markdown. This makes it, so it can be used on multiple platforms. In addition, the 
+Markdown Engine is sophisticated enough to where we can pull in data, similar to how data is pulled in using an API. 
+This allows us to have a very sophisticated, pseudo-autonomous content engine.
+
+## How It Came To Be ##
+First and foremost, we worked with LaTeX for the, at the time, standalone Angular: The Full Gamut book. 
+
+1. What we found is that LaTeX had an issue compiling at times. 
+2. For certain technical folk who never used LaTeX before, we found that there was a tremendous learning curve. 
+3. Adding LaTeX didn't allow us to modify anything. For Razroo which as an organization is very content heavy, 
+it is important that we can modify the content ourselves. LaTeX uses C++, and we wanted to be able to use 
+Typescript/Node.
+4. LaTeX to web formats seemed like a real challenge, there had to be an easier way to do this. 
+
+## Features of the Razroo Markdown Engine ##
+
+### Inject Github Links ###
+Allows Markdown to pull in github links from both internal and external repos. The Github links are then built into the 
+`build` folder. 
+
+For instance:
+```
+Sample content before. 
+
+{{ https://github.com/facebook/react/blob/ad6f3d5c55a79e8a44798aad36118e73de3a64f8/packages/create-subscription/index.js#L10-L12 }}
+
+Sample content after.
+```
+turns into:
+
+```
+Sample content before. 
+
+'use strict';
+
+export * from './src/createSubscription';
+
+Sample content after.
+``` 
+
+
---
+++ index.js
@@ -0,0 +1,73 @@
+require('dotenv').config()
+const micromatch = require("micromatch");
+const url = require("url");
+const a = require("axios");
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
+const argv = yargs(hideBin(process.argv)).argv;
+const fs = require("fs");
+var path = require("path");
+const axios = a.create({
+  headers: {'Authorization': `token ${process.env.GITHUB_TOKEN}`}
+})
+async function replaceSnippets(fileAsArray) {
+  // glob matching pattern
+  let match = micromatch(fileAsArray, ["*{{**}}*"]);
+  let promises = [];
+
+  for (x in match) {
+    let promise = new Promise(async (resolve, reject) => {
+      // remove the braces
+      let indexOfCode = fileAsArray.indexOf(match[x]);
+      match[x] = match[x].replace("{{ ", "");
+      match[x] = match[x].replace(" }}", "");
+
+      // we need to work with raw content from github
+      match[x] = match[x].replace("/blob", "");
+      match[x] = match[x].replace("github.com", "raw.githubusercontent.com");
+
+      //   get the line numbers
+      let lineNumbers = url.parse(match[x]).hash;
+
+      //   remove the Ls and #
+      lineNumbers = lineNumbers.replace("L", "");
+      lineNumbers = lineNumbers.replace("L", "");
+      lineNumbers = lineNumbers.replace("#", "");
+      lineNumbers = lineNumbers.split("-");
+
+      //   get the github raw content
+      await axios
+        .get(match[x])
+        .then(function (response) {
+          // handle success
+          let fileArray = response.data.split("\n");
+          let codeSnippet = fileArray.slice(lineNumbers[0] - 1, lineNumbers[1]);
+          codeSnippet.unshift("```");
+          codeSnippet.push("```");
+          fileAsArray[indexOfCode] = codeSnippet.join("\n");
+          resolve(codeSnippet);
+        })
+        .catch(function (error) {
+          console.log(error);
+          reject(error);
+        });
+    });
+    promises.push(promise);
+  }
+  return await Promise.all(promises).then((v) => {
+    return fileAsArray;
+  });
+}
+
+exports.resolveMarkdownFile = function (inputFilePath, outputFilePath) {
+  return new Promise((resolve, reject) => {
+    replaceSnippets(fs.readFileSync(inputFilePath, "utf8").split("\n"))
+      .then((newFileArray) => {
+        fs.writeFileSync(outputFilePath, newFileArray.join("\n"));
+        resolve(outputFilePath);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+};
