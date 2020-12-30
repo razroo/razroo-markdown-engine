bce60e10de1b942af3a63cd6396be6503052a4de
Charlie Greenman <charlie@razroo.com>
2020-12-30 20:58:09 +0200

    Remove deadcode from the Razroo markdown engine.

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,5 @@
 require('dotenv').config();
-const micromatch = require('micromatch');
-const url = require('url');
-import axios from 'axios';
 import { readFileSync, writeFileSync } from 'fs';
-import {matchInjectedLinks, modifyGithubLink, removeCurlyBraces, removeLsandHashtags} from "./inject-link/inject-link";
-
-const authenticatedAxios = axios.create({
-  headers: { Authorization: `token ${process.env.GITHUB_TOKEN}` },
-});
-async function replaceSnippets(fileAsArray: string) {
-  // glob matching pattern
-  let match = matchInjectedLinks(fileAsArray);
-  let promises = [];
-
-  for (const x in match) {
-    const promise = new Promise(async (resolve, reject) => {
-      // remove the braces
-      const indexOfCode = fileAsArray.indexOf(match[x]);
-      match[x] = removeCurlyBraces(match[x]);
-      // we need to work with raw content from github
-      match[x] = modifyGithubLink(match[x]);
-
-      //   get the line numbers
-      let lineNumbers = removeLsandHashtags(match[x]);
-
-      //   get the github raw content
-      await authenticatedAxios
-        .get(match[x])
-        .then((response: any) => {
-          // handle success
-          let fileArray = response.data.split('\n');
-          let codeSnippet = fileArray.slice(lineNumbers[0] - 1, lineNumbers[1]);
-          codeSnippet.unshift('```');
-          codeSnippet.push('```');
-          fileAsArray[indexOfCode] = codeSnippet.join('\n');
-          resolve(codeSnippet);
-        })
-        .catch((error: any) => {
-          console.log(error);
-          reject(error);
-        });
-    });
-    promises.push(promise);
-  }
-  return await Promise.all(promises).then((v) => {
-    return fileAsArray;
-  });
-}
 
 exports.resolveMarkdownFile = (inputFilePath: any, outputFilePath: any) => {
   return new Promise((resolve, reject) => {
--- src/inject-link/inject-link.ts
+++ src/inject-link/inject-link.ts
@@ -1,26 +1,6 @@
-const url = require('url');
-const micromatch = require('micromatch');
 import remark = require("remark");
 import embeddedCodeSnippets from '@razroo/razroo-remark-embed-code';
-const report = 'vfile-reporter';
 require('dotenv').config();
-
-export function removeLsandHashtags(match: any) {
-    //   get the line numbers
-    let lineNumbers = url.parse(match).hash;
-
-    //   remove the Ls and #
-    lineNumbers = lineNumbers.replace('L', '');
-    lineNumbers = lineNumbers.replace('L', '');
-    lineNumbers = lineNumbers.replace('#', '');
-    lineNumbers = lineNumbers.split('-');
-
-    return lineNumbers;
-}
-
-export function matchInjectedLinks(fileAsArray: string): string[] {
-  return micromatch(fileAsArray, ['*{{**}}*']);
-}
 
 export function testCodeInjection(markdownAsString: string) {
   remark()
@@ -33,13 +13,4 @@ export function testCodeInjection(markdownAsString: string) {
     .process(markdownAsString, (err, file) => {
         console.log(String(file));
     });
-}
-
-
-export function removeCurlyBraces(match: any) {
-  return match.replace('{{ ', '').replace(' }}', '');
-}
-
-export function modifyGithubLink(match: any) {
-  return match.replace('/blob', '').replace('github.com', 'raw.githubusercontent.com');
 }
