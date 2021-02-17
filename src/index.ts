3573a6d785731a007631d039cfa4c8eeb5d94144
Charlie Greenman <charlie@razroo.com>
2021-02-17 23:43:35 +0200

    Add remark parse and see if that does it.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import remark = require("remark");
 import embeddedCodeSnippets from "@razroo/razroo-remark-embed-code";
 const remarkInclude = require('@karuga/remark-include');
 const variables = require('remark-variables');
+const remarkParse = require('remark-parse');
 require('dotenv').config();
 
 import { readFileSync, writeFileSync } from 'fs';
@@ -18,6 +19,7 @@ exports.resolveMarkdownFile = (fileToBeBuilt: string, outputFilePath: string, da
         username: 'razroo',
         token: `${process.env.GITHUB_TOKEN}`,
       })
+      .use(remarkParse)
       .use(remarkInclude, { cwd: __dirname, glob: true, escaped: true })
       .data('var', data)
       .process(markdownAsString, (err, file) => {
