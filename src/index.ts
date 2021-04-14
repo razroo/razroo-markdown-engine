5a310252f072b519c122a02caf482639d9f42da7
Charlie Greenman <charlie@razroo.com>
2021-04-15 01:47:24 +0300

    Add prism to the repo.

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ const variables = require('remark-variables');
 require('dotenv').config();
 import { readFileSync, writeFileSync } from 'fs';
 import html = require('remark-html');
+import prism = require('remark-prism');
 import removeTitleTagsPlugin from '@razroo/razroo-remark-netlify';
 
 exports.resolveMarkdownFile = (fileToBeBuilt: string, outputFilePath: string, data: any) => {
@@ -19,6 +20,7 @@ exports.resolveMarkdownFile = (fileToBeBuilt: string, outputFilePath: string, da
         username: 'razroo',
         token: `${process.env.GITHUB_TOKEN}`,
       })
+      .use(prism)
       .use(html)
       .data('var', data)
       .process(markdownAsString, (err, file) => {
